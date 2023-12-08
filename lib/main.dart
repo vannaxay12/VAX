ad2f116cac49f47dc7854430f657470fa8f45ece
Your Name <you@example.com>
2023-12-08 11:39:52 +0700

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'MyWidget.dart';
+import 'homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,8 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyWidget2(),
+      home: homepage(),
     );
   }
 }
